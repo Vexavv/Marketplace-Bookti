25397c65d55d6a22f14435f7cc00dec0004c58c6
Vitalii <vexavv2022@gmail.com>
2023-11-27 14:33:29 +0200

    fix grid

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import styles from './Banner.module.scss'
 import Container from "../Container/Container";
 import Button from "../Button/Button";
+import {useTranslation} from "react-i18next";
+
 const Banner = () => {
+    const {t} = useTranslation('home')
     return (
         <div className={styles.Banner}>
             <Container>
-                <h1 className={styles.BannerText}>
-                    Спілкуйся за допомогою сторінок, поділися своєю історією
-                </h1>
-                <img className={styles.BannerImg} src="/home/OBJECTS.png" alt="book"/>
-                <div>
-                    <Button name='BannerButton'>Додати книгу</Button>
-                </div>
+                <div className={styles.BannerWrapper}>
+                    <h1 className={styles.BannerWrapperText}>
+                        {t('Banner.slogan')}
+                    </h1>
+                    <div className={styles.BannerWrapperImage}>
+                        <img className={styles.BannerWrapperImageImg} src="/home/OBJECTS.png" alt="book"/>
+                    </div>
+                    <div className={styles.BannerWrapperButton}>
+                        <Button name='BannerButton'>{t('Banner.button')}</Button>
+                    </div>
 
+                </div>
             </Container>
         </div>
     );
