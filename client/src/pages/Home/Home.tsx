40ef88304a9b31df532a06a710d46feabe2f0fbb
Vitalii <vexavv2022@gmail.com>
2023-11-28 17:04:21 +0200

    fin

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import styles from './Home.module.scss'
 import Banner from "../../components/Banner/Banner";
 import Search from "../../components/Search/Search";
 import Description from "../../components/Description/Description";
+import HowWork from "../../components/HowWork/HowWork";
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Search/>
             <Description/>
+            <HowWork/>
         </div>
     );
 };
