81efc0b564fe3b09c8913d348ff4607458837068
Vitalii <vexavv2022@gmail.com>
2023-11-28 23:46:54 +0200

    start

---
+++ client/src/components/CallToLogin/CallToLogin.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './CallToLogin.module.scss'
+import {useTranslation} from "react-i18next";
+const CallToLogin = () => {
+    const {t} = useTranslation('home')
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CallToLogin;
