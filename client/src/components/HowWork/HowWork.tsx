c0efb070cf87a1655323cc4903ee5491980c53c3
Vitalii <vexavv2022@gmail.com>
2023-11-28 10:03:32 +0200

    start

---
+++ client/src/components/HowWork/HowWork.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './HowWork.module.scss'
+const HowWork = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HowWork;
