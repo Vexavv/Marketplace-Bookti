8e85228adfe8a8f293e2b207f06930e5e3145af8
Vitalii <vexavv2022@gmail.com>
2024-01-11 11:52:01 +0200

    start communication

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ import MobileMenu from "./MobileMenu/MobileMenu";
 import Logo from "./Logo/Logo";
 import {Link} from "react-router-dom";
 import PopperUser from "../MyAccountComponent/PopperUser/PopperUser";
+import Communication from "./Сommunication/Сommunication";
 
 const Header = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -21,7 +22,7 @@ const Header = () => {
         <header className={styles.Header}>
             <Container>
                 <div className={styles.HeaderWrapper}>
-                <Link to='/'>
+                    <Link to='/'>
                         <Logo/>
                     </Link>
 
@@ -30,8 +31,11 @@ const Header = () => {
 
                     <div className={styles.HeaderWrapperMenu}>
                         {/*<div className={styles.HeaderWrapperMenuNav}>*/}
-                            <Navigation/>
+                        <Navigation/>
                         {/*</div>*/}
+                        <div>
+                            <Communication/>
+                        </div>
 
 
                         <div>
---
+++ client/src/components/Header/Сommunication/Сommunication.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Сommunication.module.scss'
+const Communication = () => {
+    return (
+        <div>
+            Icon
+        </div>
+    );
+};
+
+export default Communication;
