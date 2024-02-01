c249f610935c68fe6c8b1cd0938c4da2b3205898
Vitalii <vexavv2022@gmail.com>
2024-02-01 19:21:54 +0200

    add rename page

---
+++ client/src/pages/RenamePassword/RenamePassword.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './RenamePassword.module.scss'
+const RenamePassword = () => {
+    return (
+        <div>
+            ChangePassword
+        </div>
+    );
+};
+
+export default RenamePassword;
