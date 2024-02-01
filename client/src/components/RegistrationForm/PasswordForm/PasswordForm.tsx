b29984733c3bf906dd5e5b9e8745cb1ea4130b0f
Vitalii <vexavv2022@gmail.com>
2024-02-01 20:50:36 +0200

    add rename form

---
+++ client/src/components/RegistrationForm/PasswordForm/PasswordForm.tsx
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import styles from './PasswordForm.module.scss'
+import {useTranslation} from "react-i18next";
+import {Formik, Form, Field, ErrorMessage, FormikHelpers,} from 'formik';
+import * as yup from "yup";
+import YupPassword from 'yup-password'
+import {LoginForm} from "../../../types";
+import {IoEyeOffOutline, IoEyeOutline} from "react-icons/io5";
+import Button from "../../../uiComponent/Button/Button";
+import logo from "../../Header/Logo/Logo";
+
+
+interface PasswordValues {
+    password: string,
+    confirm_password: string,
+
+
+} // initialValues
+const initialValuesPassword: PasswordValues = {
+    password: '',
+    confirm_password: '',
+
+}
+const PasswordForm = () => {
+    const {t} = useTranslation('login')
+    const [isShowPassword, setIsShowPassword] = useState(false)
+    const [isShowConfirm, setIsShowConfirm] = useState(false)
+    //----------------------------Validation-----------------------------------------------------------
+    YupPassword(yup)
+    const validationSchemaPassword: yup.Schema<LoginForm> = yup.object().shape({
+        password: yup.string()
+            .minSymbols(0)
+            .min(8, t('Error.login.password.min'))
+            .max(20, t('Error.login.password.max'))
+            .minLowercase(1, t('Error.login.password.minLowercase'))
+            .minUppercase(1, t('Error.login.password.minUppercase'))
+            .minNumbers(1, t('Error.login.password.minNumbers'))
+            .required(t('Error.login.email.required')),
+        confirm_password: yup.string()
+            .oneOf([yup.ref('password')], t('Error.registration.confirmPassword.oneOf'))
+            .required(t('Error.login.email.required')),
+
+    })
+
+    const handleToggle = (setData: React.Dispatch<React.SetStateAction<boolean>>) => {
+        setData((prev: boolean) => !prev);
+    };
+
+
+    return (
+        <div>
+            <Formik initialValues={initialValuesPassword}
+                    validationSchema={validationSchemaPassword}
+                    onSubmit={async (values: PasswordValues, {resetForm}: FormikHelpers<PasswordValues>) => {
+                        await console.log(values)
+                        resetForm();
+                    }}>
+                <Form className={styles.Form}>
+                    <div className={styles.FormVisibilityWrapper}>
+                        <Field
+                            className={styles.FormInput}
+                            name="password"
+                            type={isShowPassword ? 'text' : 'password'}
+                            placeholder={t('LoginPlaceholder.newPassword')}
+                        />
+                        <ErrorMessage className={styles.FormInputError} component="span" name="password"/>
+                        <span className={styles.FormVisibilityWrapperVisibility}
+                              onClick={() => {
+                                  handleToggle(setIsShowPassword)
+                              }}>
+                                        {isShowPassword ?
+                                            <IoEyeOutline className={styles.FormVisibilityWrapperVisibilityIcon}/> :
+                                            <IoEyeOffOutline className={styles.FormVisibilityWrapperVisibilityIcon}/>}
+                                    </span>
+                    </div>
+                    <div className={styles.FormVisibilityWrapper}>
+                        <Field
+                            className={styles.FormInput}
+                            name="confirm_password"
+                            type={isShowConfirm ? 'text' : 'password'}
+                            placeholder={t('RegistrationPlaceholder.confirmPassword')}
+                        />
+                        <ErrorMessage className={styles.FormInputError} component="span" name="confirm_password"/>
+                        <span className={styles.FormVisibilityWrapperVisibility}
+                              onClick={() => {
+                                  handleToggle(setIsShowConfirm)
+                              }}>
+                                        {isShowConfirm ?
+                                            <IoEyeOutline className={styles.FormVisibilityWrapperVisibilityIcon}/> :
+                                            <IoEyeOffOutline className={styles.FormVisibilityWrapperVisibilityIcon}/>}
+                                    </span>
+                    </div>
+                    <div className={styles.FormButton}>
+                        <Button name='MobileMenu' type="submit">{t('RenameButton')}</Button>
+                    </div>
+
+                </Form>
+
+            </Formik>
+        </div>
+    );
+};
+
+export default PasswordForm;
--- client/src/pages/RenamePassword/RenamePassword.tsx
+++ client/src/pages/RenamePassword/RenamePassword.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import styles from './RenamePassword.module.scss'
+import {useTranslation} from "react-i18next";
+import Container from "../../uiComponent/Container/Container";
+import {Link} from "react-router-dom";
+import PasswordForm from "../../components/RegistrationForm/PasswordForm/PasswordForm";
+
+
 const RenamePassword = () => {
+
+    const {t} = useTranslation('login')
     return (
-        <div>
-            ChangePassword
-        </div>
+        <>
+            <Container>
+                <div className={styles.Wrapper}>
+                    <div className={styles.WrapperArrow}>
+                        <Link className={styles.WrapperArrowLink} to='/'> <img src="/login/arrow.svg"
+                                                                               alt="arrow"/>{t('arrow')}</Link>
+                    </div>
+                    <div className={styles.WrapperForm}>
+                        <p className={styles.WrapperFormGreetings}>{t('titleRenamePassword')}</p>
+                        <p className={styles.WrapperFormText}>{t('textRenamePassword')}</p>
+                        <PasswordForm/>
+                    </div>
+                    <div className={styles.WrapperImage}>
+                        <img src="/login/books.png" alt="books"/>
+                    </div>
+                </div>
+            </Container>
+
+        </>
     );
 };
 
