2d691c6697146ca1168de550859fc57013c959a3
Vitalii <vexavv2022@gmail.com>
2024-03-16 20:03:46 +0200

    added styles

--- client/src/components/MySubscriptionsComponent/MySubscriptionsUsers/MySubscriptionsUsers.tsx
+++ client/src/components/MySubscriptionsComponent/MySubscriptionsUsers/MySubscriptionsUsers.tsx
@@ -5,7 +5,7 @@ import {User} from "../../../types";
 
 const mySubscriptionsUsers: User[] = [
     {id: 1, avatar_url: "", full_name: "Вася Семенов", city: "Kyiv", email: ""},
-    {id: 2, avatar_url: "", full_name: "Олена Коваль", city: "Kyiv", email: ""}
+    {id: 2, avatar_url: "/test/photoUser.png", full_name: "Олена Коваль", city: "Kyiv", email: ""}
 ]
 const MySubscriptionsUsers = () => {
     return (
@@ -14,8 +14,8 @@ const MySubscriptionsUsers = () => {
                 <li className={styles.MyUsersItem} key={item.id}>
                     <div className={styles.MyUsersItemContent}>
                         <div className={styles.MyUsersItemContentAvatar}>
-                            {item.avatar_url ? <img src={item.avatar_url} alt="user-avatar"/>
-                                : <img src="/header/user.svg" alt="user"/>
+                            {item.avatar_url ? <img src={item.avatar_url} alt="user-avatar" style={{ width: '50px', height: '50px' }}/>
+                                : <img src="/header/user.svg" alt="user" style={{ width: '40px', height: '40px' }}/>
                             }
                         </div>
                         <div className={styles.MyUsersItemContentText}>
