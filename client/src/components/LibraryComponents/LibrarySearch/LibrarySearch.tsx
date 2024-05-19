009c619a0f4131e50af2c2dc4d9d9b2ca9421156
Vitalii <vexavv2022@gmail.com>
2024-05-19 15:09:15 +0300

    added search

--- client/src/components/LibraryComponents/LibrarySearch/LibrarySearch.tsx
+++ client/src/components/LibraryComponents/LibrarySearch/LibrarySearch.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './LibrarySearch.module.scss'
+import {useTranslation} from "react-i18next";
+import Button from "../../../uiComponent/Button/Button";
 const LibrarySearch = () => {
+    const {t} = useTranslation('library')
+    const [search, setSearch] = useState('')
+    const handleSearchChange = (event:any) => {
+        setSearch(event.target.value);
+    };
+    const handleSubmit = async (event:any) => {
+        event.preventDefault();
+
+    };
     return (
-        <div>
-           <input/>
+        <div className={styles.Search}>
+            <img className={styles.SearchImage} src="/library/layers.svg" alt="img"/>
+            <form className={styles.SearchForm} onSubmit={handleSubmit}>
+                <input className={styles.SearchFormInput} onChange={handleSearchChange}  type='text'
+                       value={search} placeholder={t('Placeholder')}/>
+                <Button  type='submit'>click</Button>
+            </form>
         </div>
     );
 };
